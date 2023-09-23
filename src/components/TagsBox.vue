abda13987d194d2023bfd6b6aee650b7ed0d8e94
happice0746 <w62459770@163.com>
2023-09-23 22:42:50 +0800

    chore: add vue-grid-layout

--- src/components/TagsBox.vue
+++ src/components/TagsBox.vue
@@ -1,12 +1,11 @@
 <script>
-import { Container, Draggable } from 'vue3-smooth-dnd'
+import { GridItem, GridLayout } from 'vue-grid-layout'
 import Kxz from '@/assets/ico/kxz.png'
-import { applyDrag } from '@/utils/helpers'
 
 export default ({
   components: {
-    Container,
-    Draggable,
+    GridLayout,
+    GridItem,
   },
   data() {
     return {
@@ -14,6 +13,24 @@ export default ({
       name: '',
       open: false,
       tags: [],
+      layout: [
+        { x: 3, y: 0, w: 1, h: 3, i: '0' },
+        { x: 3, y: 0, w: 1, h: 3, i: '1' },
+        { x: 2, y: 0, w: 1, h: 3, i: '2' },
+        { x: 2, y: 0, w: 1, h: 3, i: '3' },
+        { x: 4, y: 0, w: 1, h: 3, i: '4' },
+        { x: 4, y: 0, w: 1, h: 3, i: '5' },
+        { x: 5, y: 0, w: 1, h: 3, i: '6' },
+        { x: 5, y: 0, w: 1, h: 3, i: '7' },
+        { x: 6, y: 0, w: 1, h: 3, i: '8' },
+        { x: 6, y: 0, w: 1, h: 3, i: '9' },
+        { x: 7, y: 0, w: 1, h: 3, i: '10' },
+        { x: 7, y: 0, w: 1, h: 3, i: '11' },
+        { x: 8, y: 0, w: 1, h: 3, i: '12' },
+        { x: 8, y: 0, w: 1, h: 3, i: '13' },
+        { x: 9, y: 0, w: 1, h: 3, i: '14' },
+        { x: 9, y: 0, w: 1, h: 3, i: '15' },
+      ],
     }
   },
   mounted() {
@@ -27,7 +44,6 @@ export default ({
       this.open = true
     },
     handleOk(e) {
-      console.debug(e)
       if (this.url === '')
         return
 
@@ -62,27 +78,42 @@ export default ({
 </script>
 
 <template>
-  <Container
-    class="tagsBox" :get-ghost-parent="getGhostParent" :remove-on-drop-out="true" @drop="onDrop"
-    @drop-ready="onDropReady"
-  >
-    <Draggable v-for="tag in tags" :key="tag.key" class="tagBox" @click="openTag(tag)">
-      <div class="ico" @contextmenu.prevent.stop="rightClick(tag)">
-        <img :src="tag.icon" class="tagIcon">
-      </div>
-      <div class="tagName">
-        {{ tag.name }}
-      </div>
-    </Draggable>
-    <div class="tagBox">
-      <div class="ico">
-        <div id="addTag" class="tagIcon" @click="showModal" />
-      </div>
-      <div class="tagName">
-        添加标签
-      </div>
+  <div>
+    <div class="tagsBox">
+      <GridLayout
+        v-model:layout="layout" :col-num="12" :row-height="30" :is-draggable="true" :is-resizable="true"
+        :is-mirrored="false" :vertical-compact="true" :margin="[10, 10]" :use-css-transforms="true"
+      >
+        <GridItem
+          v-for="item in layout" :key="item.i" :x="item.x" :y="item.y" :w="item.w" :h="item.h" :i="item.i"
+          class="itemBox"
+        >
+          <div class="ico" @contextmenu.prevent.stop="rightClick(tag)">
+            <img class="tagIcon">
+          </div>
+          <div class="tagName">
+            123456
+          </div>
+        </GridItem>
+      </GridLayout>
+      <!-- <div v-for="tag in tags" class="tagBox" @click="openTag(tag)">
+                <div class="ico" @contextmenu.prevent.stop="rightClick(tag)">
+                    <img :src=tag.icon class="tagIcon">
+                </div>
+                <div class="tagName">
+                    {{ tag.name }}
+                </div>
+            </div>
+            <div class="tagBox">
+                <div class="ico">
+                    <div class="tagIcon" id="addTag" @click="showModal"></div>
+                </div>
+                <div class="tagName">
+                    添加标签
+                </div>
+            </div> -->
     </div>
-  </Container>
+  </div>
   <div>
     <a-modal v-model:open="open" title="添加标签" @ok="handleOk">
       <a-space direction="vertical" class="add">
